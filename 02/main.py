2f4086e9ec6e1efd1acdb5dc3959dfca84dff4af
Marvin Irmler <m.irmler@hm.edu>
2023-10-21 07:56:47 +0200

    day 02 part two

--- 02/main.py
+++ 02/main.py
@@ -30,3 +30,37 @@ if __name__ == '__main__':
 
     print(f'Your score is {score}')
     print(f'(inputs read: {inputs_read})')
+
+# part two
+
+    data = open("/Users/mavger22/PycharmProjects/Advent_of_Code_2022/02/data.txt")
+    score = 0
+    inputs_read = 0
+    while True:
+
+        read = data.readline()
+        read = read[:3]
+
+        if read == "":
+            break
+
+        inputs_read += 1
+
+        me = read[2]
+
+        if me == "Y":
+            score += 3
+        elif me == "Z":
+            score += 6
+
+        if read == "B X" or read == "A Y" or read == "C Z":
+            score += 1
+        elif read == "C X" or read == "B Y" or read == "A Z":
+            score += 2
+        elif read == "A X" or read == "C Y" or read == "B Z":
+            score += 3
+        else:
+            Exception("invalid operation")
+
+    print(f'Your score is {score}')
+    print(f'(inputs read: {inputs_read})')
