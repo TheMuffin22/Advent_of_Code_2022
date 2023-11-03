e2c0b3ca8b95a72d962ed676a8c648123908f9e5
Marvin Irmler <m.irmler@hm.edu>
2023-11-03 23:13:49 +0100

    day 03 part two

--- 03/main.py
+++ 03/main.py
@@ -4,6 +4,13 @@ def find_common_item(string):
             return string[i]
 
 
+def find_group_label(group):
+    group.sort(key=len, reverse=True)
+    for i in range(len(group[0])):
+        if group[0][i] in group[1] and group[0][i] in group[2]:
+            return group[0][i]
+
+
 def evaluate_priority(character):
     if 96 < ord(character) < 123:
         return ord(character) - 96
@@ -29,3 +36,26 @@ if __name__ == '__main__':
         print(prio)
         sum_prio += prio
     print(f'Your priority sum is {sum_prio} ({lines} bags evaluated)')
+
+    # second half
+
+    data = open("/Users/mavger22/PycharmProjects/Advent_of_Code_2022/03/data.txt")
+    sum_prio = 0
+    groups = 0
+    while True:
+        group = []
+        for i in range(3):
+            group.append(data.readline())
+
+        if group[0] == "":
+            break
+
+        label = find_group_label(group)
+        prio = evaluate_priority(label)
+
+        sum_prio += prio
+        groups += 1
+
+    print(f'Your group label sum is {sum_prio} ({groups} groups evaluated)')
+
+
